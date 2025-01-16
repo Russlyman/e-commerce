fa84b8b39e21c24504ba3041e39cc20e789a4507
Ross Elliman <rosselliman@hotmail.com>
2025-01-16 16:50:58 +0000

    Basic item page

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -8,10 +9,13 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=50)
     SKU = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=100)
+    image = CloudinaryField("image", default="placeholder")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
+
 
 class Wishlist(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlist")
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import *
 
 
 # Create your views here.
 def index(request):
     return render(request, "shop/index.html")
+
+def product(request, product_id):
+    product_query = get_object_or_404(Product, id=product_id)
+    return render(request, "shop/product.html", {"product": product_query})
