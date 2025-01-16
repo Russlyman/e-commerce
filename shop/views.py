c2d18e82af356ea8300f542367ed73b22e0e275e
Ross Elliman <rosselliman@hotmail.com>
2025-01-16 12:21:27 +0000

    HTML, CSS, JS boilerplate

--- shop/views.py
+++ shop/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(response):
-    return HttpResponse("Hello world!")
+def index(request):
+    return render(request, "shop/index.html")
