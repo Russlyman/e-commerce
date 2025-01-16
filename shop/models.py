b493860b90a9d60f6568cd3138313bdebbdd15d2
Ross Elliman <rosselliman@hotmail.com>
2025-01-16 12:04:56 +0000

    Add OrderProduct model

--- shop/models.py
+++ shop/models.py
@@ -20,4 +20,8 @@ class Wishlist(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
     order_date = models.DateTimeField(null=True, blank=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="products")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="orders")
+    quantity = models.IntegerField()
