b1d59cff125b4981ff3a55d48c6f1d27fc33061d
Meryem Essalhi <meryem.essalhi@gmail.com>
2025-01-17 12:28:08 +0000

    Added view cart and remove from cart functionnalities

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,12 @@
 from . import views
 from django.urls import path
 
+
+
 urlpatterns = [
     path("", views.index, name="home"),
     path('category/<int:category_id>/', views.item_category, name='item_category'),
+    path('cart/', views.view_cart, name='cart'),
+    path('remove_from_cart/<int:order_product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path("item/<int:product_id>", views.product, name="product"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import *
 
@@ -9,33 +9,34 @@ def index(request):
 
 def product(request, product_id):
     product_query = get_object_or_404(Product, id=product_id)
-    reviews = product.ratings.all().order_by("-created_at")
-    rating_count = product.ratings.count()
-    avg_rating = product.ratings.aggregate(Avg('rating'))['rating__avg'] or 0
-    stars = range(1, 6)
-    user_rating = None
-    rating_form = None
+    # reviews = product.ratings.all().order_by("-created_at")
+    # rating_count = product.ratings.count()
+    # avg_rating = product.ratings.aggregate(Avg('rating'))['rating__avg'] or 0
+    # stars = range(1, 6)
+    # user_rating = None
+    # rating_form = None
 
-    if request.user.is_authenticated:
-        user_rating = Rating.objects.filter(product=product, user=request.user).first()
+    # if request.user.is_authenticated:
+    #     user_rating = Rating.objects.filter(product=product, user=request.user).first()
         
-        if request.method == "POST":
-            if user_rating:
-                messages.info(request, "You've already reviewd this product. You can't submit another review.")
-            else:
-                rating_form = RatingForm(data=request.POST)
-                if rating_form.is_valid():
-                    rating = rating_form.save(commit=False)
-                    rating.product = product
-                    rating.user = request.user
-                    rating.save()
-                    messages.success(request, 'Your rating has been submitted successfully!')
-            return redirect('shop:product', product_id = product_id)
-        else:
-            rating_form = RatingForm() if not user_rating else None
+    #     if request.method == "POST":
+    #         if user_rating:
+    #             messages.info(request, "You've already reviewd this product. You can't submit another review.")
+    #         else:
+    #             rating_form = RatingForm(data=request.POST)
+    #             if rating_form.is_valid():
+    #                 rating = rating_form.save(commit=False)
+    #                 rating.product = product
+    #                 rating.user = request.user
+    #                 rating.save()
+    #                 messages.success(request, 'Your rating has been submitted successfully!')
+    #         return redirect('shop:product', product_id = product_id)
+    #     else:
+    #         rating_form = RatingForm() if not user_rating else None
     return render(request, "shop/product.html", {"product": product_query})
 
 def item_category(request, category_id):
+    # cat=Category.select_related("products")
     category = get_object_or_404(Category, id=category_id)
     products = category.products.all() 
     
@@ -43,3 +44,20 @@ def item_category(request, category_id):
         'category': category,
         'products': products,
     })
+
+def view_cart(request):
+    order = Order.objects.filter(customer=request.user, order_date__isnull=True).first()
+    if not order:
+        return HttpResponse("Your cart is empty.")
+    order_products = OrderProduct.objects.filter(order=order)
+    return render(request, 'shop/cart.html', {
+        'order': order,
+        'order_products': order_products
+    })
+
+# remove product from cart
+def remove_from_cart(request, order_product_id):
+    order_product = get_object_or_404(OrderProduct, id=order_product_id)
+    order_product.delete()
+    return redirect('cart')
+
