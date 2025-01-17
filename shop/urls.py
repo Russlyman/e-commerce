74f844e7c7d0d16e5adafbf8ea0dc24e655f1ace
Ross Elliman <rosselliman@hotmail.com>
2025-01-17 16:33:14 +0000

    Add items to basket

--- shop/urls.py
+++ shop/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.index, name="home"),
     path('cart/', views.view_cart, name='cart'),
     path('remove_from_cart/<int:order_product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path("product/<int:product_id>", views.product, name="product"),
     path("category/<int:category_id>", views.category, name="category"),
     path('wishlist/', views.view_wishlist, name='wishlist'),
