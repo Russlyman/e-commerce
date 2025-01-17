f7de18fddbaa94ca1004b1112f78d1c0a28c72a0
Ross Elliman <rosselliman@hotmail.com>
2025-01-17 10:16:04 +0000

    Fix product page for main

--- shop/views.py
+++ shop/views.py
@@ -9,30 +9,30 @@ def index(request):
 
 def product(request, product_id):
     product_query = get_object_or_404(Product, id=product_id)
-    reviews = product.ratings.all().order_by("-created_at")
-    rating_count = product.ratings.count()
-    avg_rating = product.ratings.aggregate(Avg('rating'))['rating__avg'] or 0
+    # reviews = product.ratings.all().order_by("-created_at")
+    # rating_count = product.ratings.count()
+    # avg_rating = product.ratings.aggregate(Avg('rating'))['rating__avg'] or 0
     stars = range(1, 6)
     user_rating = None
     rating_form = None
 
-    if request.user.is_authenticated:
-        user_rating = Rating.objects.filter(product=product, user=request.user).first()
+    # if request.user.is_authenticated:
+    #     user_rating = Rating.objects.filter(product=product, user=request.user).first()
         
-        if request.method == "POST":
-            if user_rating:
-                messages.info(request, "You've already reviewd this product. You can't submit another review.")
-            else:
-                rating_form = RatingForm(data=request.POST)
-                if rating_form.is_valid():
-                    rating = rating_form.save(commit=False)
-                    rating.product = product
-                    rating.user = request.user
-                    rating.save()
-                    messages.success(request, 'Your rating has been submitted successfully!')
-            return redirect('shop:product', product_id = product_id)
-        else:
-            rating_form = RatingForm() if not user_rating else None
+    #     if request.method == "POST":
+    #         if user_rating:
+    #             messages.info(request, "You've already reviewd this product. You can't submit another review.")
+    #         else:
+    #             rating_form = RatingForm(data=request.POST)
+    #             if rating_form.is_valid():
+    #                 rating = rating_form.save(commit=False)
+    #                 rating.product = product
+    #                 rating.user = request.user
+    #                 rating.save()
+    #                 messages.success(request, 'Your rating has been submitted successfully!')
+    #         return redirect('shop:product', product_id = product_id)
+    #     else:
+    #         rating_form = RatingForm() if not user_rating else None
     return render(request, "shop/product.html", {"product": product_query})
 
 def item_category(request, category_id):
